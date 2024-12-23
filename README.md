b310fc0c5d847d30bf8983002aac700e7811b045
Alex <alexeytimonov1@gmail.com>
2024-12-23 19:34:32 +0200

    add links, specify language, limit news and subject

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is the project?
+This app accesses news to some topic and send them by email
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import requests
+from send_email import send_email
 
-api_key = "efafa33301d84109b0581e484bd8459d"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-11-23&sortBy=publishedAt&apiKey=efafa33301d84109b0581e484bd8459d"
+api_key = "890603a55bfa47048e4490069ebee18c"
+url = "https://newsapi.org/v2/everything?q=tesla&" \
+      "sortBy=publishedAt&apiKey=" \
+      "890603a55bfa47048e4490069ebee18c&language=en"
 
+# Make request
 request = requests.get(url)
 
+# Get a dictionary with data
 content = request.json()
 
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+# Access the article titles and description
+body = ""
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = "Subject: New daily mail" + "\n" + body + article["title"] \
+                + "\n" + str(article["description"]) + "\n" \
+                + str(article["url"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
